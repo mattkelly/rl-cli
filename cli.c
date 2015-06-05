2defded35d489c5435e875bb34a9df616704a56e
Matt Kelly <Matthew.Joseph.Kelly@gmail.com>
2015-06-04 23:51:27 -0400

    Implement basic help functionality, add another test command

--- cli.c
+++ cli.c
@@ -1,16 +1,12 @@
 #include<readline/readline.h>
 #include<readline/history.h>
 
+#include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 #define PROMPT "> "
-
-enum {
-    MAX_COMMAND_NAME_LEN = 128,
-    MAX_INFO_LEN = 2048,
-};
 
 typedef int (*command_function_t)(void);
 
@@ -19,17 +15,72 @@ typedef struct
     char *name;
     command_function_t func;
     char *info;
+    char *info_long;
 } command_t;
+
+/**
+ * Command line functions
+ */
+static int _cmd_help(void);
+static int _cmd_test(void);
+
+/**
+ * NOTE: Lexicographical order must be maintained for this list
+ * in order to facilitate searching.
+ */
+static const command_t _command_registry[] = {
+    {"help", _cmd_help, "Print help.",
+        "This command prints the help message."},
+    {"test", _cmd_test, "Test function.",
+        "This is a test function. I'm not sure what\n"
+        "it should do yet. But right here I'm just\n"
+        "testing the output of long info strings."},
+
+    {(char*)NULL, NULL, (char*)NULL} // end of table marker
+};
 
 static int _cmd_help()
 {
+    int i;
+
+    printf("\nThis is an example help messsage.\n\n");
+
+    for (i = 0; _command_registry[i].name; ++i)
+    {
+        printf("%s: %s\n", _command_registry[i].name, _command_registry[i].info);
+    }
+
     return 0;
 }
 
-static const command_t _command_registry[] = {
-    {"help", _cmd_help, "Print help."},
-    {(char*)NULL, NULL, (char*)NULL}
-};
+static int _cmd_test()
+{
+    printf("Test function called!\n");
+    return 0;
+}
+
+static int _execute_command(char *cmd)
+{
+    int i;
+
+    // TODO for now no support for anything useful e.g. args,
+    // just run a command if it is an exact match
+    i = 0;
+    while(_command_registry[i].name)
+    {
+        if (!strcmp(_command_registry[i].name, cmd))
+        {
+            // Ensure uniqueness
+            if (_command_registry[i + 1].name && !strcmp(_command_registry[i + 1].name, cmd))
+                return -1; // TODO
+            else
+                return (*_command_registry[i].func)();
+        }
+        i++;
+    }
+
+    return -1;
+}
 
 static void _interactive()
 {
@@ -40,7 +91,7 @@ static void _interactive()
         line  = readline(PROMPT); 
         if (line)
         {
-            printf("%s\n", line);
+            _execute_command(line);
             free(line);
         }
     }
